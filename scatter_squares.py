1b778d65495f5ba49b367f0e1b0318c27d44d125
John Adams Jr <john.c.adams32@gmail.com>
2021-12-23 10:53:02 -0700

    Customizing the colors and adding plot save function

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,18 +1,19 @@
 from matplotlib import pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
 
 plt.style.use('Solarize_Light2')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=100)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Reds, s=10)
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
 
-# Set the size of tick labels.
-ax.tick_params(axis='both', which='major', labelsize=14)
-plt.show()
+# Set the range for each axis.
+ax.axis([0, 1100, 0, 1100000])
 
+plt.show()
+plt.savefig('squares_plot.png', bbox_inches='tight')
