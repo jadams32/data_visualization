0ec7225e71e9b0c48ac9803cc4fd5bb311b6a611
John Adams Jr <john.c.adams32@gmail.com>
2021-12-23 10:20:10 -0700

    adding style and correcting input x values

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,9 +1,11 @@
 from matplotlib import pyplot as plt
 
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 
+plt.style.use('ggplot')
 fig, ax = plt.subplots()
-ax.plot(squares, linewidth=3)
+ax.plot(input_values, squares, linewidth=3)
 
 # Set Chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
