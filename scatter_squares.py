26bb68d19c27926a0085e652e63638ad9a9b9984
John Adams Jr <john.c.adams32@gmail.com>
2021-12-23 11:17:15 -0700

    Additonal comments, and changing the plot to plot the first 5000 cubic numbers

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,8 +1,13 @@
+# Here I recreate the scatter plot project from the book python crash course.
+
+# Importing pyplot and giving it the alis plt
 from matplotlib import pyplot as plt
 
-x_values = range(1, 1001)
-y_values = [x**2 for x in x_values]
+# X and Y values using list comprehension and range
+x_values = range(1, 5001)
+y_values = [x**3 for x in x_values]
 
+# styling and color maps for the plot
 plt.style.use('Solarize_Light2')
 fig, ax = plt.subplots()
 ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Reds, s=10)
@@ -13,7 +18,8 @@ ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
 
 # Set the range for each axis.
-ax.axis([0, 1100, 0, 1100000])
+ax.axis([0, 5100, 0, 150000000000])
 
+# Here we show the plot and then save a copy in the same folder
 plt.show()
 plt.savefig('squares_plot.png', bbox_inches='tight')
