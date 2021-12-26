c8c06499b088a06330f5ebe40e919a2c05eda161
John Adams Jr <john.c.adams32@gmail.com>
2021-12-26 11:34:00 -0700

    Visualizing a d6 and d10 die roll using a histogram

--- die_visual.py
+++ die_visual.py
@@ -31,4 +31,4 @@ x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Result'}
 my_layout = Layout(title='Results of rolling a D6 and a D10  50000 times', xaxis=x_axis_config,
                    yaxis=y_axis_config)
-offline.plot({'data': data, 'layout': my_layout}, filename='d6_d6.html')
+offline.plot({'data': data, 'layout': my_layout}, filename='d6_d10.html')
