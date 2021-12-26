595b556841e8fc10286ad6d34de5f75aba60b6ca
John Adams Jr <john.c.adams32@gmail.com>
2021-12-26 10:42:43 -0700

    Finished random walk

--- random_walk.py
+++ random_walk.py
@@ -4,7 +4,7 @@
 from random import choice
 
 class Randomwalk:
-    """A cLass to generate random walks."""
+    """A class to generate random walks."""
 
     def __init__(self, num_points=5000):
         """Initialize attributes of a walk."""
--- rw_visual.py
+++ rw_visual.py
@@ -5,17 +5,22 @@ from random_walk import Randomwalk
 
 while True:
     # Make a random Walk
-    rw = Randomwalk()
+    rw = Randomwalk(5_000)
     rw.fill_walk()
+
     # Plot the points in the walk.
     plt.style.use('classic')
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(10, 5), dpi=128)
     point_numbers = range(rw.num_points)
-    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=3)
 
     # Emphasize the first and last points
     ax.scatter(0, 0, c='green', edgecolors='none', s=100)
     ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+    # Remove the axis.
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
     plt.show()
 
     run_again = input("Make another walk? (Y/N): ").lower()
