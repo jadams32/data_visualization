19adb276587418f9ca9a75578cb594f4a3b8a556
John Adams Jr <john.c.adams32@gmail.com>
2021-12-26 11:08:09 -0700

    Testing of the roll function

--- die_visual.py
+++ die_visual.py
@@ -1,3 +1,23 @@
 # Visualization for the roll of dice. (die.py file)
 
 from die import Die
+
+#Create a D6.
+die = Die()
+
+# Make some rolls, and store results in a list.
+results = []
+
+for roll_num in range(1000):
+    result = die.roll()
+    results.append(result)
+
+print(results)
+
+# Analyze the results.
+frequencies = []
+for value in range(1, die.num_sides + 1):
+        frequency = results.count(value)
+        frequencies.append(frequency)
+
+print(frequencies)
