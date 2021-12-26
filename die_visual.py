c37548340a6e32469b95535638ed780c7d247ba5
John Adams Jr <john.c.adams32@gmail.com>
2021-12-26 11:25:44 -0700

    Visualizing the die roll using a histogram

--- die_visual.py
+++ die_visual.py
@@ -1,8 +1,9 @@
 # Visualization for the roll of dice. (die.py file)
-
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
 from die import Die
 
-#Create a D6.
+# Create a D6.
 die = Die()
 
 # Make some rolls, and store results in a list.
@@ -12,7 +13,7 @@ for roll_num in range(1000):
     result = die.roll()
     results.append(result)
 
-print(results)
+# print(results)
 
 # Analyze the results.
 frequencies = []
@@ -21,3 +22,13 @@ for value in range(1, die.num_sides + 1):
     frequencies.append(frequency)
 
 print(frequencies)
+
+# Visualize the results.
+x_values = list(range(1, die.num_sides + 1))
+data = [Bar(x=x_values, y=frequencies)]
+
+x_axis_config = {'title': 'Result'}
+y_axis_config = {'title': 'Frequency of Result'}
+my_layout = Layout(title='Results of rolling one D6 1000 times', xaxis=x_axis_config,
+                   yaxis=y_axis_config)
+offline.plot({'data': data, 'layout': my_layout}, filename='d6.html')
