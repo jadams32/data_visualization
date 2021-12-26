2c12fba0b78f5c84f8dd7f8ead51a182e5e608b3
John Adams Jr <john.c.adams32@gmail.com>
2021-12-26 11:31:06 -0700

    Visualizing two die roll using a histogram

--- die_visual.py
+++ die_visual.py
@@ -3,32 +3,32 @@ from plotly.graph_objs import Bar, Layout
 from plotly import offline
 from die import Die
 
-# Create a D6.
-die = Die()
+# Create two D6.
+die_1 = Die()
+die_2 = Die()
 
 # Make some rolls, and store results in a list.
 results = []
 
 for roll_num in range(1000):
-    result = die.roll()
+    result = die_1.roll() + die_2.roll()
     results.append(result)
-
-# print(results)
 
 # Analyze the results.
 frequencies = []
-for value in range(1, die.num_sides + 1):
+max_result = die_1.num_sides + die_2.roll()
+for value in range(2, max_result + 1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
 print(frequencies)
 
 # Visualize the results.
-x_values = list(range(1, die.num_sides + 1))
+x_values = list(range(2, max_result + 1))
 data = [Bar(x=x_values, y=frequencies)]
 
-x_axis_config = {'title': 'Result'}
+x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Result'}
-my_layout = Layout(title='Results of rolling one D6 1000 times', xaxis=x_axis_config,
+my_layout = Layout(title='Results of rolling two D6 dice 1000 times', xaxis=x_axis_config,
                    yaxis=y_axis_config)
-offline.plot({'data': data, 'layout': my_layout}, filename='d6.html')
+offline.plot({'data': data, 'layout': my_layout}, filename='d6_d6.html')
