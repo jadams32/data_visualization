5d2f85d84f61c4bf6538459b8ed413280efd27a5
John Adams Jr <john.c.adams32@gmail.com>
2021-12-26 11:33:39 -0700

    Visualizing a d6 and d10 die roll using a histogram

--- die_visual.py
+++ die_visual.py
@@ -3,14 +3,14 @@ from plotly.graph_objs import Bar, Layout
 from plotly import offline
 from die import Die
 
-# Create two D6.
+# Create a D6 and a d10.
 die_1 = Die()
-die_2 = Die()
+die_2 = Die(10)
 
 # Make some rolls, and store results in a list.
 results = []
 
-for roll_num in range(1000):
+for roll_num in range(50_000):
     result = die_1.roll() + die_2.roll()
     results.append(result)
 
@@ -29,6 +29,6 @@ data = [Bar(x=x_values, y=frequencies)]
 
 x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Result'}
-my_layout = Layout(title='Results of rolling two D6 dice 1000 times', xaxis=x_axis_config,
+my_layout = Layout(title='Results of rolling a D6 and a D10  50000 times', xaxis=x_axis_config,
                    yaxis=y_axis_config)
 offline.plot({'data': data, 'layout': my_layout}, filename='d6_d6.html')
